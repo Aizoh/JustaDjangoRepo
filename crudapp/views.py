9a1f780012a0dcf6b9a4e4733ad9a8f9bcf52aea
Aizoh <ghetaohmaina2@gmail.com>
2023-10-02 21:29:03 +0300

    Added retreival to frontend of items and images

--- crudapp/views.py
+++ crudapp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from item.models import Category, Item
 
 # Create your views here.
 def index(request):
-    return render (request, 'crudapp/index.html')
+    #get first 6 unsold items
+    items = Item.objects.filter(is_sold = False)[0:6]
+    #get categories
+    categories = Category.objects.all()
+
+    context = {
+        'categories' : categories,
+        'items' : items
+    }
+    return render (request, 'crudapp/index.html', context)
 
 def contact(request):
     return render (request, 'crudapp/contact.html')
